6b942fee6939379af81433ecbc9469d82a6c0ce3
Vikas Solanki <vikassolankics@gmail.com>
2019-10-01 19:34:52 +0530

    Create HelloWorld.scala

---
+++ HelloWorld.scala
@@ -0,0 +1,14 @@
+//************** Object Oriented Approach ******************
+
+package Demo
+
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("hello world")
+  }
+}
+//************** Functional Approach ******************
+def scalaExample{  
+    println("Hello Scala")  
+}  
+scalaExample  
