53ed5f7018de86c2bd8b15aec4e7732e70508453
Vikas Solanki <vikassolankics@gmail.com>
2019-10-03 16:03:55 +0530

    Update JoinStart.scala

--- Multihreading/JoinStart.scala
+++ Multihreading/JoinStart.scala
@@ -3,7 +3,7 @@ package Multihreading
 class JoinStart extends Thread{
   override def run() = {
     for(i<- 1 to 5) {
-      println(i)
+      println(this.getName + i)
       Thread.sleep(500)
     }
   }
@@ -13,6 +13,9 @@ object JoinStart {
     var t1 = new JoinStart
     var t2 = new JoinStart
     var t3 = new JoinStart
+    t1.setName("First Thread - ")
+    t2.setName("Second Thread - ")
+    t3.setName("Third Thread - ")
     t1.start()
     t1.join()
     t2.start()
